2433b07fedbdd201b28b49b0c7fab2ae2f1ff0e8
omego <mserihi@gmail.com>
2022-01-16 13:59:05 +0300

    Fixed gulp version with macos update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
+    sass = require('gulp-sass')(require('sass')),
     cleanCss = require('gulp-clean-css'),
     sourcemaps = require('gulp-sourcemaps'),
     clean = require('gulp-clean'),
